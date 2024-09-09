6d9d45cd638a41d86a8a216ba11a280aea67cff5
John Thompson <john@springframework.guru>
2024-09-09 14:48:41 -0400

    refactor to use ChatModel

--- src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
@@ -2,10 +2,9 @@ package guru.springframework.springairag.services;
 
 import guru.springframework.springairag.model.Answer;
 import guru.springframework.springairag.model.Question;
-import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
@@ -17,13 +16,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    final ChatClient chatClient;
+    private final ChatModel chatModel;
 
     @Override
     public Answer getAnswer(Question question) {
         PromptTemplate promptTemplate = new PromptTemplate(question.question());
         Prompt prompt = promptTemplate.create();
-        ChatResponse response = chatClient.call(prompt);
+        ChatResponse response = chatModel.call(prompt);
 
         return new Answer(response.getResult().getOutput().getContent());
     }
