4c7848096eb30528aa4ea813f3a4ec05245a3eb8
John Thompson <john@springframework.guru>
2025-01-01 08:08:07 -0500

    refactor to search request builder for deprecation in M5.

--- src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
@@ -33,8 +33,8 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Override
     public Answer getAnswer(Question question) {
 
-        List<Document> documents = vectorStore.similaritySearch(SearchRequest
-                .query(question.question()).withTopK(5));
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
+                .query(question.question()).topK(5).build());
         List<String> contentList = documents.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
